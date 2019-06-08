c03d128808b7df3b055de2efb3825dd63105ba3e
Willian Duarte <willianm.cduarte@gmail.com>
2019-06-07 21:38:22 -0300

    creating estructure

---
+++ src/br/edu/qi/persistency/DatabaseConnection.java
@@ -0,0 +1,53 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Brian & Willian.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.edu.qi.persistency;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Brian & Willian
+ * @since 07/06/2019 - 18:16:18
+ * @version 1.0
+ */
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/XXXX";
+    private static final String USER = "XXXX";
+    private static final String PASSWORD = "XXXX";
+
+    public static Connection getConexao() throws SQLException {
+
+        Connection c = null;
+        try {
+            c = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException se) {
+            throw new SQLException("Erro ao conectar no Banco de Dados! " + se.getMessage());
+        }
+
+        return c;
+    }
+}
