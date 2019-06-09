944d64a5e0e4b6e16721770e10c116631419c43c
Willian Duarte <willianm.cduarte@gmail.com>
2019-06-09 18:49:33 -0300

    creating OrderListVO

---
+++ src/br/edu/qi/model/OrderListVO.java
@@ -0,0 +1,101 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Brian & Willian.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package br.edu.qi.model;
+
+/**
+ *
+ * @author Brian & Willian
+ * @since 09/06/2019 - 17:43:42
+ * @version 1.0
+ */
+public class OrderListVO {
+
+    private OrderVO orderVO;
+    private BurguerVO burguerVO;
+    private int quantity;
+    private String observation;
+
+    public OrderListVO() {
+    }
+
+    public OrderListVO(OrderVO orderVO, BurguerVO burguerVO, int quantity, String observation) {
+        this.orderVO = orderVO;
+        this.burguerVO = burguerVO;
+        this.quantity = quantity;
+        this.observation = observation;
+    }
+
+    public OrderVO getOrderVO() {
+        return orderVO;
+    }
+
+    public void setOrderVO(OrderVO orderVO) {
+        this.orderVO = orderVO;
+    }
+
+    public BurguerVO getBurguerVO() {
+        return burguerVO;
+    }
+
+    public void setBurguerVO(BurguerVO burguerVO) {
+        this.burguerVO = burguerVO;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+    
+    /**
+     * Return the subtotal of the order
+     * @return Quantity times the price of the burguer.
+     */
+    public double getSubtotal() {
+        return this.quantity * this.burguerVO.getPrice();
+    }
+
+    @Override
+    public String toString() {
+        return "\n"
+                + "OrderID: " + orderVO.getIDOrder() + "\n"
+                + "Burguer: " + burguerVO.getName() + "\n"
+                + "Quantity: " + quantity + "\n"
+                + "Observation: " + observation + "\n"
+                + "Subtotal: " + getSubtotal() + "\n";
+    }
+    
+    
+}
