50d242528c731dcbc09ad95867ca3d8ba2f31010
Willian Duarte <willianm.cduarte@gmail.com>
2019-06-08 23:54:58 -0300

    creating BurguerDAO

--- src/br/edu/qi/dao/DAOFactory.java
+++ src/br/edu/qi/dao/DAOFactory.java
@@ -33,9 +33,19 @@ package br.edu.qi.dao;
 public class DAOFactory {
     
     private static final ClientDAO CLIENT_DAO = new ClientDAO();
+    private static final BurguerDAO BURGUER_DAO = new BurguerDAO();
+    private static final OrderDAO ORDER_DAO = new OrderDAO();
 
     public static ClientDAO getClientDAO() {
         return CLIENT_DAO;
     }
 
+    public static BurguerDAO getBurguerDAO() {
+        return BURGUER_DAO;
+    }
+
+    public static OrderDAO getOrderDAO() {
+        return ORDER_DAO;
+    }
+    
 }
