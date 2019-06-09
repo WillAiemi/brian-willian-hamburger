0e613a596341542682846cec2b51f51d87b4392e
Willian Duarte <willianm.cduarte@gmail.com>
2019-06-09 16:52:49 -0300

    Implemented all core methods for OrderDAO

--- src/br/edu/qi/dao/DAOFactory.java
+++ src/br/edu/qi/dao/DAOFactory.java
@@ -31,7 +31,6 @@ package br.edu.qi.dao;
  * @version 1.0
  */
 public class DAOFactory {
-    private static final OrderDAO ORDER_DAO = new OrderDAO();
     private static final ClientDAO CLIENT_DAO = new ClientDAO();
     private static final BurguerDAO BURGUER_DAO = new BurguerDAO();
     private static final OrderDAO ORDER_DAO = new OrderDAO();
